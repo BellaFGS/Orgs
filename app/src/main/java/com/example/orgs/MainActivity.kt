a014a8feaa22428640c4e37b2211d3d66786ef5e
Isabellafgs <ferreiraisabella1216@gmail.com>
2025-03-26 11:12:57 -0300

    Hello, World

--- app/src/main/java/com/example/orgs/MainActivity.kt
+++ app/src/main/java/com/example/orgs/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.example.orgs
 
 import android.app.Activity
+import android.os.Bundle
+import android.widget.Toast
 
-class MainActivity : Activity {
+class MainActivity : Activity() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Toast.makeText(this, "Hello, World!", Toast.LENGTH_SHORT).show()
+    }
 }
