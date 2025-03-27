e95c02f8b3c84fe2191fdf5b0fda5562b35f431f
Isabellafgs <ferreiraisabella1216@gmail.com>
2025-03-27 11:20:00 -0300

    layout

--- app/src/main/java/com/example/orgs/MainActivity.kt
+++ app/src/main/java/com/example/orgs/MainActivity.kt
@@ -2,12 +2,12 @@ package com.example.orgs
 
 import android.app.Activity
 import android.os.Bundle
-import android.widget.Toast
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Toast.makeText(this, "Hello, World!", Toast.LENGTH_SHORT).show()
+        setContentView(R.layout.activity_main)
+
     }
 }
